74511706765c55b1937c1ad88db3c69bf34f9bdb
Aiden <aiden.chettleburgh720@gmail.com>
2025-09-25 12:20:35 -0400

    Implemented retrive method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -91,8 +91,22 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        // If path is empty, retrieve the root of this triangle
+        if (path.isEmpty()) {
+            return this.root;
+        }
+        // Recursive call on left sub-triangle
+        else if (path.charAt(0) == 'l') {
+            return this.left.retrieve(path.substring(1));
+        }
+        // Recursive call on right sub-triangle
+        else if (path.charAt(0) == 'r') {
+            return this.right.retrieve(path.substring(1));
+        }
+        // Path is invalid
+        else {
+            return -1;
+        }
     }
 
     /** Read in the NumberTriangle structure from a file.
